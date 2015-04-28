55356e991b2a336dfe02d762f7c139f2cd1ac042
Petr Janda <petrjandacz@gmail.com>
2015-04-28 21:42:59 +0100

    add derivatives for the functions

--- core/src/main/scala/nn/fn/loss/LossFunction.scala
+++ core/src/main/scala/nn/fn/loss/LossFunction.scala
@@ -4,4 +4,6 @@ import org.nd4j.linalg.api.ndarray.INDArray
 
 trait LossFunction {
   def apply(labels: INDArray, output: INDArray): Double
+
+  def derivative(y: INDArray, a: INDArray): INDArray
 }
