c3b0b8d21e037df5ea1b0130dba9c0a78590d592
Petr Janda <petrjandacz@gmail.com>
2015-05-01 20:59:43 +0100

    add repository

--- core/src/main/scala/nn/fn/act/ActivationFunction.scala
+++ core/src/main/scala/nn/fn/act/ActivationFunction.scala
@@ -3,6 +3,6 @@ package nn.fn.act
 import org.apache.commons.math3.random.MersenneTwister
 import org.nd4j.linalg.api.ndarray.INDArray
 
-trait ActivationFunction {
+trait ActivationFunction extends Serializable {
   def apply(x: INDArray)(implicit rng:MersenneTwister): INDArray
 }
--- core/src/test/scala/nn/RBMTest.scala
+++ core/src/test/scala/nn/RBMTest.scala
@@ -7,6 +7,7 @@ import nn.fn.act.Sigmoid
 import nn.fn.learn.ConstantRate
 import nn.fn.loss.CrossEntropy
 import nn.trainer.RBMTrainer
+import nn.util.Repo
 import org.apache.commons.math3.random.MersenneTwister
 import org.scalatest.{FlatSpec, Matchers}
 import org.slf4j.LoggerFactory
@@ -17,16 +18,20 @@ class RBMTest extends FlatSpec with Matchers {
   implicit val rng = new MersenneTwister(123)
 
   val trainer = RBMTrainer(
-    epochs = 10,
+    epochs = 1,
     miniBatchSize = 25,
     learningRate = ConstantRate(0.05)
   )
 
+  val repo = new Repo("src/test/resources/nets/")
+
   it should "get reasonable loss after learning with ones as targets" in {
-    val mnist = MNIST.read("data/mnist/train-labels-idx1-ubyte", "data/mnist/train-images-idx3-ubyte", Some(1000))
+    val mnist = MNIST.read("data/mnist/train-labels-idx1-ubyte", "data/mnist/train-images-idx3-ubyte", Some(1))
     val ds = DataSet(mnist._1.transpose)
 
     val l1 = trainer.train(RBM(784, 500, Sigmoid, Sigmoid, CrossEntropy), ds)
+
+    repo.save(l1, "l1.o")
 
     val l2ds = DataSet(l1.propUp(ds.inputs))
     val l2 = trainer.train(RBM(500, 200, Sigmoid, Sigmoid, CrossEntropy), l2ds)
@@ -37,6 +42,9 @@ class RBMTest extends FlatSpec with Matchers {
     val rbm = MultiLayerRBM(List(l1, l2, l3), CrossEntropy)
 
     val d = trainer.train(RBM(784, 50, Sigmoid, Sigmoid, CrossEntropy), ds)
+
+    repo.save(rbm, "rbm-multi.o")
+    repo.save(rbm, "rbm-single.o")
 
     println("deep: " + rbm.loss(ds.inputs))
     println("direct: " + d.loss(ds.inputs))
