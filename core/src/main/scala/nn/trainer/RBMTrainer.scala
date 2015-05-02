40f0785d83690e5a0669acbf074bf165b27aafc1
Petr Janda <petrjandacz@gmail.com>
2015-05-02 11:58:48 +0100

    clean empty line

--- core/src/main/scala/nn/trainer/RBMTrainer.scala
+++ core/src/main/scala/nn/trainer/RBMTrainer.scala
@@ -11,7 +11,6 @@ case class RBMTrainer(epochs: Int, miniBatchSize: Int, learningRate:LearningFunc
     val iterations = dataSet.numExamples * epochs / miniBatchSize
 
     dataSet.miniBatches(miniBatchSize).take(iterations).zipWithIndex.foldLeft(rbm) { (rbm, i) =>
-
       if(i._2 % 100 == 0) log.info(s"--> ${i._2}, ${rbm.loss(dataSet.inputs)}")
 
       rbm.update(ContrastiveDivergence.diff(rbm, i._1, 1).rate(learningRate(i._2)))
